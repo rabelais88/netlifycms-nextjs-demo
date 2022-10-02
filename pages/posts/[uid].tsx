c6549b9ec1cf41d48e09da9558881684de3fee76
박성렬 <sungryeol@sr-mbp2022m1.local>
2022-10-02 14:58:42 +0700

    markdown render feature for posts/[uid]

--- pages/posts/[uid].tsx
+++ pages/posts/[uid].tsx
@@ -1,21 +1,56 @@
-import { GetServerSideProps, NextPage } from 'next';
+import {
+  GetServerSideProps,
+  GetStaticPaths,
+  GetStaticProps,
+  NextPage,
+} from 'next';
 import { promises as fs } from 'fs';
 import path from 'path';
+import { serialize } from 'next-mdx-remote/serialize';
+import { MDXRemote, MDXRemoteSerializeResult } from 'next-mdx-remote';
 
-const PostsUid: NextPage = () => {
-  return <div></div>;
+interface IPostFrontmatter {
+  title: string;
+  date: string;
+}
+
+const PostsUid: NextPage<IPostsUidProps> = ({ mdxSource, post }) => {
+  return (
+    <div>
+      <h1>{post.title}</h1>
+      <MDXRemote {...mdxSource} />
+    </div>
+  );
 };
 
-interface IPostsUidProps {}
+interface IPostsUidProps {
+  mdxSource: MDXRemoteSerializeResult;
+  post: IPostFrontmatter;
+}
 
-export const getServerSideProps: GetServerSideProps<IPostsUidProps> = async ({
+export const getStaticPaths: GetStaticPaths = async ({}) => {
+  const folderPath = path.join(process.cwd(), 'content', 'posts');
+  const filePaths = await (
+    await fs.readdir(folderPath)
+  ).map((filePath) => filePath.slice(0, -3));
+  return {
+    paths: filePaths.map((fp) => ({ params: { uid: fp } })),
+    fallback: 'blocking',
+  };
+};
+
+export const getStaticProps: GetStaticProps<IPostsUidProps> = async ({
   params,
 }) => {
   const uid = `${params?.uid ?? ''}`;
-  const filePath = path.join('content', 'posts', `${uid}.mdx`);
+  const filePath = path.join('content', 'posts', `${uid}.md`);
   const content = await fs.readFile(filePath, 'utf8');
-  console.log(content);
-  return { props: {} };
+  const { frontmatter, ...mdxSource } = await serialize(content, {
+    parseFrontmatter: true,
+  });
+  const post = frontmatter as unknown as IPostFrontmatter;
+  post.date = post.date.toString();
+  return { props: { mdxSource, post } };
 };
 
 export default PostsUid;
